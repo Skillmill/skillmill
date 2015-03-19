62d4da5ed537a49d291301afcae987443fef7a26
Stephan Yu <Stephan.yu@gmail.com>
2015-03-19 13:55:57 +0100

    added projects and designer links to navbar. fixed carrierwave/fog issue with aws s3. added thumbnails to project index page

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -7,19 +7,3 @@ CarrierWave.configure do |config|
   }
   config.fog_directory      = ENV["fog_directory"]                         # required
 end
-
-# CarrierWave.configure do |config|
-#     config.storage    =  :aws                  # required
-#     config.aws_bucket =  ENV['S3_BUCKET']      # required
-#     config.aws_acl    =  :public_read
-
-#     config.aws_credentials = {
-#       access_key_id:      ENV['S3_KEY'],       # required
-#       secret_access_key:  ENV['S3_SECRET']     # required
-#     }
-
-#     config.aws_attributes = {
-#                               'Cache-Control'=>"max-age=#{365.day.to_i}",
-#                               'Expires'=>'Tue, 29 Dec 2015 23:23:23 GMT'
-#                             }
-#   end
