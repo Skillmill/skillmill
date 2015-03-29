c6dc05be3b5258a896e592ce9984908b458c0899
Stephan Yu <Stephan.yu@gmail.com>
2015-03-29 22:25:39 +0200

    updated review form with clickable star rating. added review date and reviewer name to reviews table on show design page

--- app/controllers/designs_controller.rb
+++ app/controllers/designs_controller.rb
@@ -28,7 +28,12 @@ class DesignsController < ApplicationController
   # GET /designs/1.json
   def show
     # binding.pry
-    @reviews = Review.where(design_id: @design.id)
+    @reviews = Review.where(design_id: @design.id).order("created_at DESC")
+    if @reviews.blank?
+      @avg_rating = 0
+    else
+      @avg_rating = @reviews.average(:rating).round(2)
+    end
   end
 
   # GET /designs/new
