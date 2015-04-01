8f4049b0ac5c901067f727bcb8c870e9f5273faa
Stephan Yu <Stephan.yu@gmail.com>
2015-04-01 19:42:07 +0200

    added _navbarmenu partial to make it easier to oversee code. added categories dynamically to navbarmenu. added a Category.delete_all to the seeds.rb file to avoid conflicts when changing category names.

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,4 @@
 class Category < ActiveRecord::Base
   has_many :projects
-  validates :category_id, presence: true
+  # validates :category_id, presence: true
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,7 @@
 
-main_categories = ["Architecture", "Graphic Design", "Web Design", "Audio & Music", "Film & Video", "Photography", "Writing", "Software Development", "Other"]
+main_categories = ["Architecture", "Graphic Design", "Web Design", "Audio & Music", "Film & Video", "Photography", "Writing", "Web Development", "Other"]
 
+Category.delete_all
 main_categories.each do |category|
   Category.find_or_create_by(name: category)
 end
