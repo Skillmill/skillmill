efe2d432ff481285c51fd28ae9d957c227bea87b
Stephan Yu <Stephan.yu@gmail.com>
2015-04-01 16:35:21 +0200

    added categories array and ruby code to seeds.rb file. then seeded the database categories with category names

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,2 +1,7 @@
 
+main_categories = ["Architecture", "Graphic Design", "Web Design", "Audio & Music", "Film & Video", "Photography", "Writing", "Software Development", "Other"]
+
+main_categories.each do |category|
+  Category.find_or_create_by(name: category)
+end
 
