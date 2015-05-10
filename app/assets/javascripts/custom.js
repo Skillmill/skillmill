15c6a11081c2943885320bcae0edfa5e5993718b
Stephan Yu <Stephan.yu@gmail.com>
2015-05-10 12:53:04 +0200

    added events div to home page. created custom.js page for jquery function linked to benefits div. divided all home page divs into their separate partials

---
+++ app/assets/javascripts/custom.js
@@ -0,0 +1,3 @@
+//when you click the why button, if div is hidden make hidden div 'benefits' visible and slide down else show div
+
+//when x close button is clicked make the div button slide up and hide
