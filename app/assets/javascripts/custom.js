1093242b844273ea5495d6b48bb6d049f03f02eb
Stephan Yu <Stephan.yu@gmail.com>
2015-05-18 20:09:43 +0200

    added tour presentation with horizontal jquery menu and functionality to switch between hidden divs. also fixed navbar on how it works page and added some margin to register div. removed featured in div as it is not real

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -15,4 +15,36 @@ $(document).ready(function() {
     interval: 8000
   });
 
+  $(document).on('click', '.form a', function(e) {
+    e.preventDefault();
+    e.stopPropagation; 
+    $(this).closest('ul').find('.selected').removeClass('selected');
+    $(this).parent().addClass('selected');
+  });
+
+  //if i click step 1 btn, hide all divs inside #tour-image, then show first image
+  $(document).on('click', '.step-1', function(e) {
+    e.preventDefault();
+    $('#tour-image').find('div').addClass('hidden');
+    $('.first-image').toggleClass('hidden');
+  });
+
+  //if i click step 2 btn, hide all divs inside #tour-image, then show second image
+  $(document).on('click', '.step-2', function(e) {
+    e.preventDefault();
+    $('#tour-image').find('div').addClass('hidden');
+    $('.second-image').toggleClass('hidden');
+  });
+  //if i click step 3 btn, hide all divs inside #tour-image, then show third image
+  $(document).on('click', '.step-3', function(e) {
+    e.preventDefault();
+    $('#tour-image').find('div').addClass('hidden');
+    $('.third-image').toggleClass('hidden');
+  });
+  //if i click step 4 btn, hide all divs inside #tour-image, then show fourth image
+  $(document).on('click', '.step-4', function(e) {
+    e.preventDefault();
+    $('#tour-image').find('div').addClass('hidden');
+    $('.fourth-image').toggleClass('hidden');
+  });
 });
